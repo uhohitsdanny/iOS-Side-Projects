5e717777a5dcebe1ed5782b44b00d5e3e59984cd
Danny <uhohitsdanny@gmail.com>
2018-05-29 10:18:36 -0700

    added User Entity

--- SpyMe/SpyMe/View/GameQueue/NameCreation_VC.swift
+++ SpyMe/SpyMe/View/GameQueue/NameCreation_VC.swift
@@ -7,22 +7,31 @@
 //
 
 import UIKit
+import CoreData
 
 class NameCreation_VC: UIViewController {
     
     @IBOutlet weak var nameTextField: TextFieldCustom?
     
     var player:Civilian? = nil
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         log("NameCreation_VC is loaded")
+        
     }
     
     @IBAction func submitSpyName(){
+        let context = appDelegate.persistentContainer.viewContext
+        let entity = NSEntityDescription.entity(forEntityName: "Users", in: context)
+        let newUser = NSManagedObject(entity: entity!, insertInto: context)
         let name = nameTextField?.text
+        
+        newUser.setValue(name, forKey: "spyname")
         player = Civilian(name: name!)
+        
         performSegue(withIdentifier: "jcRmSegue", sender: self)
     }
 
