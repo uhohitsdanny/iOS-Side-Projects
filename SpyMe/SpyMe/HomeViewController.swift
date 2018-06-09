c9fb5defbdc74683b16b1b36fce030446df0aaa2
Danny <uhohitsdanny@gmail.com>
2018-06-09 14:30:16 -0700

    now connected to parse server
    edit to App Delegate

--- SpyMe/SpyMe/HomeViewController.swift
+++ SpyMe/SpyMe/HomeViewController.swift
@@ -14,17 +14,33 @@ class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         log("HomeViewController")
-        let room = PFObject(className: "Room")
-        room["roomid"] = "testid"
-        room["pw"] = "testpw"
-        room["status"] = "standby"
+        //One time testing. Delete after DB is setup******
+//        let room = PFObject(className: "Room")
+//        room["roomid"] = "testid"
+//        room["pw"] = "testpw"
+//        room["status"] = "standby"
+//
+//        room.saveInBackground { (success, error) in
+//            if success{
+//                log("Parse successfully save the room data")
+//            }
+//            else{
+//                log(error ?? "Error: Parse could not save the room data" as! Error)
+//            }
+//        }
         
-        room.saveInBackground { (success, error) in
-            if success{
-                log("Parse successfully save the room data")
+        let query = PFQuery(className: "Room")
+        query.whereKey("roomid", equalTo: "testid")
+        query.findObjectsInBackground { (objects, error) in
+            if error == nil {
+                let roomid = objects![0].object(forKey: "roomid") as! String
+                log("Parse query was successful")
+                log("Found room ID " + roomid)
             }
-            else{
-                log(error ?? "Error: Parse could not save the room data" as! Error)
+            else {
+                log("Querying Room objects failed")
+                log("----------------------------")
+                print(error ?? "")
             }
         }
     }
