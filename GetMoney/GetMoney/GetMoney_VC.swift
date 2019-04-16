6e454ce3126083e659f62823dba8c66cb297dad9
Danny <uhohitsdanny@gmail.com>
2019-04-16 10:08:11 -0700

    updated

---
+++ GetMoney/GetMoney/GetMoney_VC.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  GetMoney
+//
+//  Created by Danny Navarro on 4/13/19.
+//  Copyright © 2019 Squirrel. All rights reserved.
+//
+
+import UIKit
+
+class GetMoney_VC: UIViewController {
+
+    @IBOutlet weak var numbersList : UITextView?
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GetMoney/GetMoney/NumberGen.swift
@@ -0,0 +1,33 @@
+//
+//  NumberGenerator.swift
+//  GetMoney
+//
+//  Created by Danny Navarro on 4/13/19.
+//  Copyright © 2019 Squirrel. All rights reserved.
+//
+
+import Foundation
+
+var MAXNUMBERS = 6
+
+class NumberGen
+{
+    var numbers : [Int]
+    
+    init()
+    {
+        numbers = []
+    }
+    
+}
+
+extension NumberGen
+{
+    func generate()
+    {
+        for _ in 1...MAXNUMBERS
+        {
+            //numbers.append()
+        }
+    }
+}
